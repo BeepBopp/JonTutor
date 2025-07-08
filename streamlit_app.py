fa7805437409fe3cafe67b521b2d567280a5978c
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-07-08 18:32:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+pages = {
+  st.Page("assignment.py", title="Assignment", icon="✍️"),
+  st.Page("calendar.py", title="Calendar", icon="📆"),
+  st.Page("writing_tips.py", title="Writing Tips", icon="💡"),
+}
+
+pg = st.navigation(pages)
+st.set_page_config(page_title="Jon Tutor", page_icon="📖")
+pg.run()
