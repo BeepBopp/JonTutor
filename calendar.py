2bcfe471d3ee97e45efb5d0d6d1927ae036e11f8
BeepBopp <115367530+BeepBopp@users.noreply.github.com>
2025-07-08 18:54:08 -0400

    Update calendar.py

--- calendar.py
+++ calendar.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("📆 Calendar")
 st.write("This calendar shows your progress and grades this month!")
-st.write("✅ = Essay completed, ❌ = No essay submitted)
+st.write("✅ = Essay completed, ❌ = No essay submitted")
 
 
